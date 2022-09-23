a8aaca65c35348a6feadc27a8d71d4452126ff91
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-09-23 11:22:46 +0530

    Add files via upload

---
+++ part_03/blankapp_02/js/rootController.js
@@ -0,0 +1,38 @@
+define(["ojs/ojcorerouter", 
+        "ojs/ojknockoutrouteradapter", 
+        "ojs/ojurlparamadapter", 
+        "ojs/ojarraydataprovider", 
+        "knockout", 
+        "ojs/ojnavigationlist", 
+        "ojmodulerouter-adapter", 
+        "ojmodule-element"], 
+        
+    function(CoreRouter, KnockoutRouterAdapter, UrlParamAdapter, ArrayDataProvider, ko, nav, ModuleRouterAdapter){
+
+    function rootViewModel(){
+
+        // Define the routes
+        this.routes = [
+            { path: "", redirect: "ojtable" },
+            { path: "ojtable", detail: { label: "OJET Table" } },
+            { path: "rest", detail: { label: "ReST" } },
+        ];
+        // Create ADP with partial array, excluding first redirect route
+        this.dataProvider = new ArrayDataProvider(this.routes.slice(1), {
+            keyAttributes: "path",
+        });
+        // Create the router with the routes
+        this.router = new CoreRouter(this.routes, {
+            urlAdapter: new UrlParamAdapter(),
+        });
+        // Create an observable to react to the current router state path
+        this.selection = new KnockoutRouterAdapter(this.router);
+        // Module router adapter
+        this.moduleAdapter = new ModuleRouterAdapter(this.router);
+        // Synchronize the router, causing it to go to its default route
+        this.router.sync();
+
+    }
+
+    return new rootViewModel();
+})
